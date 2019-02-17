2d8aad5016440dbacc129fde8e7ac8b862261a80
Arnav Gupta <arnav@codingblocks.com>
2019-02-17 16:00:35 +0530

    this and sameness console outputs
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

---
+++ Lecture06/this/script.js
@@ -0,0 +1,31 @@
+function tellThis() {
+  return this
+}
+
+
+console.log(tellThis())
+
+let whatIsThis = () => {
+  return this
+}
+
+let obj = {
+  t1: tellThis(),
+  t2: tellThis,
+  t3: function () {
+    return tellThis()
+  },
+  t4: function () {
+    return obj.t2()
+  },
+  t5: function () {
+    return this
+  }
+}
+
+let obj2 = {
+  t1: whatIsThis,
+  t2: () => this
+}
+
+obj2.t3 = () => this
