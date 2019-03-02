de8903c27da812aeb9814d6a3004ac9539970bd6
Arnav Gupta <arnav@codingblocks.com>
2019-03-02 16:34:17 +0530

    full todolist functionality
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- Lecture08/todojs-oneline/script.js
+++ Lecture08/todojs-oneline/script.js
@@ -2,20 +2,51 @@ $(() => {
   $(document.body)
     .append(
       $('<input>')
-        .attr('id', 'newtask')
+      .attr('id', 'newtask')
     )
     .append(
       $('<button>')
-        .text('ADD')
-        .click(() => {
-          $('#tasklist').append(
+      .text('ADD')
+      .click(() => {
+        $('#tasklist').prepend(
             $('<li>')
+            .append(
+              $('<button>')
+              .text('❌')
+              .click((ev) => {
+                $(ev.target).parent().remove()
+              })
+            )
+            .append(
+              $('<button>')
+              .attr('class', 'btn-up')
+              .text('⬆️')
+              .click((ev) => {
+                $(ev.target).parent().insertBefore(
+                  $(ev.target).parent().prev()
+                )
+              })
+            )
+            .append(
+              $('<button>')
+              .attr('class', 'btn-down')
+              .text('⬇️')
+              .click((ev) => {
+                $(ev.target).parent().insertAfter(
+                  $(ev.target).parent().next()
+                )
+              })
+            )
+            .append(
+              $('<span>')
               .text($('#newtask').val())
+            )
           )
-        })
+          .prev().prev().val('')
+      })
     )
     .append(
       $('<ul>')
-        .attr('id', 'tasklist')
+      .attr('id', 'tasklist')
     )
 })
