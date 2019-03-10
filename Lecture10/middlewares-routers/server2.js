c0ec49441db358977cce9aeca7e4a69292d1459a
Arnav Gupta <arnav@codingblocks.com>
2019-03-10 19:27:47 +0530

    quoz
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- Lecture10/middlewares-routers/server2.js
+++ Lecture10/middlewares-routers/server2.js
@@ -2,18 +2,22 @@ const app = require('express')()
 
 app.use('/a', (req, res, next) => {
   console.log('/a')
+  res.body = '--a--'
   next()
 })
 app.use('/a/b', (req, res, next) => {
   console.log('/a/b')
+  res.body += '--b--'
   next()
 })
 app.use('/a/c', (req, res, next) => {
   console.log('/a/c')
+  res.body += '--c--'
   next()
 })
 app.use('/a', (req, res, next) => {
   console.log('/a again!!')
+  // res.send(res.body)
   next()
 })
 app.use('/b', (req, res, next) => {
@@ -27,6 +31,11 @@ app.use('/c', (req, res, next) => {
 app.use('/d', (req, res, next) => {
   console.log('/d')
   next()
+})
+
+
+app.get('/a', (req, res) => {
+  res.send(res.body)
 })
 
 app.use((req, res) => {
